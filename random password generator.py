e3317c778ba86313dd37df394e3834845a85af0b
Shubhamsp4902 <121297735+Shubhamsp4902@users.noreply.github.com>
2022-12-23 18:33:38 +0530

    Delete random password generator.py

--- random password generator.py
+++
@@ -1,5 +0,0 @@
-import random
-passlen = int(input("Enter the length of the password"))
-a = "qwertyuiopasdfghjklzxcvbnm1234567890@#$&-+()/*:;!?~√π÷×¶∆£¢€¥^°={}\%©®™✓[]"
-b = "".join(random.sample(a,passlen))
-print(b)
