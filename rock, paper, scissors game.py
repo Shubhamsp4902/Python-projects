070bb272d466ec9de310e63ccc8c31d64d82050d
Shubhamsp4902 <121297735+Shubhamsp4902@users.noreply.github.com>
2022-12-25 01:42:47 +0530

    Update rock, paper, scissors game.py

--- rock, paper, scissors game.py
+++ rock, paper, scissors game.py
@@ -1,19 +1,18 @@
 import random
 
-# while loop is used to make the value true 
 while True:
     choices = ["rock", "paper", "scissors"]
     
     computer = random.choice(choices)
     player = None
     
-    # while loop is here used to repeat the process until user input matches the choices list
+    
     while player not in choices:
         player = input("rock, paper or scissors ").lower()       
-     # From here coding for rules begins
+     
     if player == computer:
-         print("computer: ", computer) # this will print the choices of the computer
-         print("player: ", player) # this will print the choices the user given
+         print("computer: ", computer) 
+         print("player: ", player) 
          print("Tie")
          
     elif player == "rock":
@@ -46,7 +45,7 @@ while True:
                print("player: ", player)
                print("You lost!")
                
-      #  this code is regarding asking user if they want to play this game again         
+           
     play_again = input("Play Again (yes/no)").lower()
     if play_again != "yes":
              break
